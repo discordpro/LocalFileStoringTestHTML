d128c5e80684afcf3997924e197e6b0a9038ee29
discordpro <discordpro01@gmail.com>
2018-10-03 18:12:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 window.webkitRequestFileSystem(window.TEMPORARY, 1024*1024, SaveDatFileBro);
 window.webkitRequestFileSystem(window.PERSISTENT , 1024*1024, SaveDatFileBro);
+function SaveDatFileBro(localstorage) {
+  localstorage.root.getFile("info.txt", {create: true});
+}
+function SaveDatFileBro(localstorage) {
+  localstorage.root.getFile("info.txt", {create: true}, function(DatFile) {
+    DatFile.createWriter(function(DatContent) {
+      var blob = new Blob(["Test"], {type: "text/plain"});
+      DatContent.write(blob);
+    });
+  });
+}
+}
